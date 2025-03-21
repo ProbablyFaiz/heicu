d19af864f6d8312e2a731d4fdc573e7d5212e2fb
Faiz Surani <faiz.surani@gmail.com>
2025-03-21 09:58:52 -0700

    Add missing packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+import { resolve } from "path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envDir: "../",
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 });
